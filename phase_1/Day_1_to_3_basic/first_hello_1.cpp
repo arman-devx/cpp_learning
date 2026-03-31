d9667350948189d07e92667b8bfce153413a6349
arman-devx <arman.code111@gmail.com>
2026-03-31 18:56:26 +0530

    Added comments to explain code logic

--- phase_1/Day_1_to_3_basic/first_hello_1.cpp
+++ phase_1/Day_1_to_3_basic/first_hello_1.cpp
@@ -1,6 +1,8 @@
-#include<iostream>
-int main()
+#include<iostream> //includes Input-Output library for cout
+
+int main() // execution starts exactily at main function 
 {
-    std::cout << "Hello World" ;
-    return 0;
+    //printing message to the console
+    std::cout << "Hello World" ; 
+    return 0; // Programe end succesfully
 }
--- phase_1/Day_1_to_3_basic/first_hello_2.cpp
+++ phase_1/Day_1_to_3_basic/first_hello_2.cpp
@@ -1,7 +1,10 @@
-#include<iostream>
-using namespace std;
-int main()
+#include<iostream>   // Include Input-Output library for cout
+using namespace std; // allow using cout without std::
+
+int main() // execution starts exactly at the main function
 {
+    // Printing message to the console
     cout << "Hello World!" ;
-    return 0;
+
+    return 0; // indicates successful execution
 }
