5683b0887dc10d387eadc2a7df34e1bcc35b071b
arman-devx <arman.code111@gmail.com>
2026-04-04 22:30:03 +0530

    feat: add program to find largest of three numbers

---
+++ phase_1/Day_5_if_else/largest_three_number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main () // allow use of standard library names without std::
+{
+    int num1, num2, num3 ;
+
+    cout << "Enter three number : " << endl ;
+    cin >> num1 ;
+    cin >> num2 ;
+    cin >> num3 ;
+    if (cin.fail()) // cin.fail() : check if use entered invlid input (e.g., text instead of number)
+    {
+        cout << "Error: Invalid! input, Please enter a number" << endl ;
+    } else if (num1 >= num2 && num1 >= num3)
+    {
+        cout << "largest number is : " << num1 << endl ;
+    } else if (num2 >= num3)
+    {
+        cout << "largest number is : " << num2 << endl ;
+    } else 
+    cout << "largest number is : " << num3 << endl ;
+
+    return 0;
+    
+}
