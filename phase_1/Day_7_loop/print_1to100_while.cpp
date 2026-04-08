c6d290ca6668fd7d8e36f34ca01d96ecb4a7b9fa
arman-devx <arman.code111@gmail.com>
2026-04-08 20:08:19 +0530

    feat: add program 1 to 100 numbers print form while loop

---
+++ phase_1/Day_7_loop/print_1to100_while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/*
+  program : 1 to 100 numbers print form while loop
+*/
+int main()
+{
+    int i = 1;
+    while (i <= 100)
+    {
+        cout << i << endl;
+        i++;
+    }
+}
