a3a9382b91378f830a58c37e6d887d18be88e20c
arman-devx <arman.code111@gmail.com>
2026-04-08 20:29:12 +0530

    feat: add program 1 to 100 numbers print from do-while loop

---
+++ phase_1/Day_7_loop/print_1to100_do-while.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+/*
+    program : print 1 to 100 numbers from do-while loop
+*/
+int main()
+{
+    int i = 1;
+    // do
+    // {
+    //     /* code */
+    // } while (condition);
+    
+    do
+    {
+        cout << i << endl;
+        i++;
+
+    } while (i <= 100);
+    
+}
