12785b0124eb48cc98f2320f2c8332f17c28df92
arman-devx <arman.code111@gmail.com>
2026-04-08 19:34:28 +0530

    feat: add program 1 to 100 number print from for loop

---
+++ phase_1/Day_7_loop/print_1to100_for.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+/*
+  program : print 1 to 100 number for for loop
+*/
+int main()
+{
+
+    /*
+    for (int i, i = 0; i < count; i++)
+    int i, data type and her name
+    //i = 0;        // data_type loop starting loop count condition
+    //i < count;    //data_type ending loop count condition
+    //i++ ;         //loop increas + 1
+    {
+        cpp code 
+    }
+        */
+    
+    for (int i = 1 ; i < 100; i++) // loop start
+    {
+        cout << i << endl ; //loop value print
+    }
+
+    return 0;
+    
+}
